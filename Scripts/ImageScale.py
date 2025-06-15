1c59d21890740c9244384489975a177a79b586c3
Riccardo <riccardo.pompili@lnf.infn.it>
2025-06-15 13:33:09 +0200

    updated script for stopping acquisition on prev camera

--- Scripts/ImageScale.py
+++ Scripts/ImageScale.py
@@ -2,21 +2,9 @@ from org.csstudio.display.builder.runtime.script import PVUtil, ScriptUtil
 # Access the widget (the parent of this script)
 # widget = display.getWidget("CameraImage")  # Replace with your widget's name
 
-#try:
-#    min_value = PVUtil.getDouble(pvs[0])
-#except:
-#    min_value = 0
-#    print("min_val not initialized")
-#
-#try:
-#    max_value = PVUtil.getDouble(pvs[1])
-#except:
-#    max_value = 4095
-#    print("max_val not initialized")
-
 # Get PV values (replace with your PV names)
-min_value = PVUtil.getDouble(pvs[0])
-max_value = PVUtil.getDouble(pvs[1])
+min_value = 0 #PVUtil.getDouble(pvs[0])
+max_value = PVUtil.getDouble(pvs[0])
 
 # Set colormap range
 widget.setPropertyValue("minimum", min_value)
--- Scripts/reloadpanel.py
+++ Scripts/reloadpanel.py
@@ -1,18 +1,25 @@
 from org.csstudio.display.builder.runtime.script import PVUtil, ScriptUtil
-import os
+#import os
 
 StopPrevAcq=True;
-
 logger = ScriptUtil.getLogger()
 pv = PVUtil.getString(pvs[0])
-device_actual = widget.getEffectiveMacros().getValue("DEVICE")
-#print("DEVICE: "+device_actual)
-#print("ACTUAL DEVICE '%s'" % device_actual)
 print("User selection: " + pv)
 #logger.info("SELECTION: " + pv)
 
 #if device_actual and pv:
 if pv:
+    mydev = widget.getEffectiveMacros().getValue("DEVICE")
+    old_cam = widget.getEffectiveMacros().getValue("CAM")
+    if "NULL" in old_cam or StopPrevAcq==False:
+        print("Previous camera: "+old_cam)
+        #pass;
+    else:
+        #stop acquire
+        pv00=PVUtil.createPV(mydev+":"+old_cam+":Acquire",100)
+        pv00.setValue(0)
+        print("Stopped acquisition of the previous camera: "+old_cam)
+
     vals = pv.split(" (")
     cam_name=vals[0]
     print("Camera: "+cam_name)
@@ -25,38 +32,6 @@ if pv:
     macros.add("CAM", cam_name)
     #macros.add("HW", vals[1])
     widget.setPropertyValue("file", "CompactCamera.bob")
-
-    #append the camera name to a txt file
-    home_dir = os.path.expanduser("~")
-    file_path = os.path.join(home_dir, "cam_history.txt")
-    # Read existing lines (if file exists)
-    if os.path.exists(file_path):
-        with open(file_path, "r") as file:
-            lines = file.readlines()
-    else:
-        lines = []
-
-    # Keep only last entry (so we have room for 1 old + 1 new = 2 entries)
-    lines = lines[-1:]  # last one or empty if file was empty
-
-    # Append new entry
-    lines.append(device_actual+":"+cam_name+"\n")
-
-    # Write back only last 2 entries
-    with open(file_path, "w") as file:
-        file.writelines(lines)
-
-    # Now check: if there are exactly 2 lines, print/display first one
-    if len(lines) == 2:
-        old_cam = lines[0]
-        if "NULL" in old_cam or StopPrevAcq==False:
-            pass;
-        else:
-            #stop acquire
-            old_cam=old_cam.replace("\n","")
-            pv00=PVUtil.createPV(old_cam+":Acquire",100)
-            pv00.setValue(0)
-            print("Stopped acquisition of the previous camera: "+old_cam)
 else:
 	pass
 
